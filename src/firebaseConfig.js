0dab2d25892112172d5e4407c666c0417864559c
maruf-ahmed <ahmedmaruf474@gmail.com>
2021-06-25 12:10:44 +0600

    initial setup

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,15 @@ import firebase from 'firebase';
 
 
 // Your config code goes here.
-const config = {};
+const config = {
+
+    apiKey: "AIzaSyBb8Yi30kXnIMaEUKaWhJ-hWqByU4xdGxE",
+    authDomain: "companydirectory-74eb9.firebaseapp.com",
+    projectId: "companydirectory-74eb9",
+    storageBucket: "companydirectory-74eb9.appspot.com",
+    messagingSenderId: "535708011704",
+    appId: "1:535708011704:web:8a0b21a264a3a55d93384d"
+};
 
 
 firebase.initializeApp(config);
